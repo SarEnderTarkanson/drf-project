25e373ee64d29c89f84439e35b13d8ee5e04502e
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-02 01:22:32 +0200

    added the second url and view

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -10,3 +10,12 @@ def movie_list(request):
         }
     
     return JsonResponse(data)
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name' : movie.name,
+        'description' : movie.description,
+        'active' : movie.active,
+    }
+    return JsonResponse(data)
