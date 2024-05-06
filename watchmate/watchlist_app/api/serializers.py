905c0744eecf7b0883cfab6d8f8ce449bfad615d
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-07 01:53:59 +0200

    added a nested serializer

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -2,17 +2,14 @@ from rest_framework import serializers
 from watchlist_app.models import WatchList, StreamPlatform
 
 
-class WatchListSerializer(serializers.ModelSerializer):
-    len_name = serializers.SerializerMethodField()
-    
+class WatchListSerializer(serializers.ModelSerializer):    
     class Meta:
         model = WatchList
         fields = '__all__'
-    
-    def get_len_name(self, object):
-        return len(object.title)
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+    
     class Meta:
         model = StreamPlatform
         fields = '__all__'
