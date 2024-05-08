49a4ce555bfdde83930883bb7134cebc6200fc79
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-08 03:01:55 +0200

    changed two of the views into generics

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 #from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import (WatchListAV, WatchDetailAV,
+                                     StreamPlatformAV, StreamPlatformDetailAV,
+                                     ReviewList, ReviewDetail)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch_list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='watch_detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream_list'),
-    path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    # path('stream/<int:pk>/review/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    # path('review/', ReviewList.as_view(), name='review_list'),
+    # path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
+    
     path('review/', ReviewList.as_view(), name='review_list'),
-    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
 ]
