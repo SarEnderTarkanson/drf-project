133eb79ebf98f261561edca9e094b269df79f56b
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-08 02:48:31 +0200

    updated two urls

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch_list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='watch_detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream_list'),
-    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
     path('review/', ReviewList.as_view(), name='review_list'),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
 ]
