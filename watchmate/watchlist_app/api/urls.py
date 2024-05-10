d164d963c6249d895daeffd3d886003f9f2ad585
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-11 00:41:18 +0200

    correected a few urls' ending

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -11,7 +11,7 @@ router.register('stream', StreamPlatformViewSet, basename='streamplatform')
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch_list'),
-    path('<int:pk>', WatchDetailAV.as_view(), name='watch_detail'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='watch_detail'),
     
     path('', include(router.urls)),
     #path('stream/', StreamPlatformAV.as_view(), name='stream_list'),
@@ -20,8 +20,8 @@ urlpatterns = [
     #path('review/', ReviewList.as_view(), name='review_list'),
     #path('review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
     
-    path('<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
-    path('<int:pk>/reviews', ReviewList.as_view(), name='review-list'),
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('<int:pk>/reviews/', ReviewList.as_view(), name='review-list'),
     path('review/<int:pk>/', ReviewDetail.as_view(), name='review_detail'),
     
 ]
